fbfaa7883cbef377ec9f455800c06f5a9c4b12e4
0-daedalus <armageddon_WO@mail.ru>
2022-08-02 15:00:24 +0600

    Add a gridmaker button

--- scripts.js
+++ scripts.js
@@ -1,10 +1,18 @@
 let container = document.querySelector('.container');
 
-function makeGrid(x, y){
-    for(let i = 0; i < y; i++){
+function makeGrid(x){
+
+    let grid = document.querySelectorAll('.square');
+    grid.forEach((sqr) => {
+        container.removeChild(sqr);
+    });
+
+    for(let i = 0; i < x; i++){
         for(let j = 0; j < x; j++){
             const square = document.createElement('div');
             square.classList.add('square');
+            square.style.width = `${100/x}%`;
+            square.style.height = `${100/x}%`;
             container.appendChild(square);
         }
     }
@@ -19,4 +27,14 @@ grid.forEach((sqr) => {
     sqr.addEventListener('mouseleave', () => {
         sqr.style.backgroundColor = 'white';
     })
+})
+
+///////////////////////////////////////////////////////////////////
+
+let btn = document.querySelector('#gridmaker');
+
+btn.addEventListener('click', () => {
+    let x = prompt("Number of squares per side:");
+    if(x > 100 || x < 0) x = prompt("Enter a number less than 100:");
+    makeGrid(x);
 })
